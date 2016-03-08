b782073a4946a86c977f64a121cb9066ba24a1a4
sanjya <sanjayanuwnaj@gmail.com>
2016-03-08 21:05:17 +0530

    impl

--- src/com/csa/util/MatchUtil.java
+++ src/com/csa/util/MatchUtil.java
@@ -3,6 +3,15 @@ package com.csa.util;
 import java.io.File;
 
 import com.csa.entity.Match;
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * @author sankha
@@ -27,10 +36,46 @@ public class MatchUtil {
 	}
 	
 	
-	public static Match getMatchInfoFromFile(File filePath){
+	public static Match getMatchInfoFromFile(File filePath) throws FileNotFoundException, YamlException, ParseException{
 	
 		Match m = new Match();
-		
+		YamlReader reader = new YamlReader(new FileReader("match.yaml"));
+        Object object = reader.read();
+        
+        Map map = (Map)object;
+        System.out.println(map.size());
+    
+    
+        Map info= (Map) map.get("info");
+        System.out.println(info.size());
+        
+        ArrayList<String> teams = (ArrayList<String>) info.get("teams");
+        System.out.println(info.get("teams"));
+        m.setTeamOne(teams.get(0));
+        m.setTeamTwo(teams.get(1));
+        
+        ArrayList<String> umpires = (ArrayList<String>) info.get("umpires");
+        System.out.println(umpires);
+        m.setUmprie1(umpires.get(0));
+        m.setUmprie2(umpires.get(1));
+        
+        Map toss= (Map) info.get("toss");
+        System.out.println((String) toss.get("winner"));
+        m.setTossWinningTeam((String) toss.get("winner"));
+        m.setTossDecision((String) toss.get("decision"));
+        
+        System.out.println(info.get("city"));
+        m.setCity((String) info.get("city"));
+        
+        System.err.println(info.get("dates"));
+        ArrayList<String> dates= (ArrayList<String>) info.get("dates");
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	String dateInString = dates.get(0);
+	Date date = sdf.parse(dateInString);
+        m.setMatchDate(date);
+        
+        m.setVenue((String) info.get("venue"));
 		return m;
 	}
 	
