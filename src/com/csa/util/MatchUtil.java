b6c4ff65b99c7e425ef4c2b95d114f59c1e5dde6
sanjaya <sanjayanuwanaj@gmail.com>
2016-03-09 19:55:33 +0530

    v1

--- src/com/csa/util/MatchUtil.java
+++ src/com/csa/util/MatchUtil.java
@@ -39,43 +39,7 @@ public class MatchUtil {
 	public static Match getMatchInfoFromFile(File filePath) throws FileNotFoundException, YamlException, ParseException{
 	
 		Match m = new Match();
-		YamlReader reader = new YamlReader(new FileReader("match.yaml"));
-        Object object = reader.read();
-        
-        Map map = (Map)object;
-        System.out.println(map.size());
-    
-    
-        Map info= (Map) map.get("info");
-        System.out.println(info.size());
-        
-        ArrayList<String> teams = (ArrayList<String>) info.get("teams");
-        System.out.println(info.get("teams"));
-        m.setTeamOne(teams.get(0));
-        m.setTeamTwo(teams.get(1));
-        
-        ArrayList<String> umpires = (ArrayList<String>) info.get("umpires");
-        System.out.println(umpires);
-        m.setUmprie1(umpires.get(0));
-        m.setUmprie2(umpires.get(1));
-        
-        Map toss= (Map) info.get("toss");
-        System.out.println((String) toss.get("winner"));
-        m.setTossWinningTeam((String) toss.get("winner"));
-        m.setTossDecision((String) toss.get("decision"));
-        
-        System.out.println(info.get("city"));
-        m.setCity((String) info.get("city"));
-        
-        System.err.println(info.get("dates"));
-        ArrayList<String> dates= (ArrayList<String>) info.get("dates");
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	String dateInString = dates.get(0);
-	Date date = sdf.parse(dateInString);
-        m.setMatchDate(date);
-        
-        m.setVenue((String) info.get("venue"));
+		
 		return m;
 	}
 	
