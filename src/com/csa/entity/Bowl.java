c81a1b6cf60f3d712103de590d168d991e406c24
sanjya <sanjayanuwanaj@gmail.com>
2016-03-17 15:19:54 +0530

    test

--- src/com/csa/entity/Bowl.java
+++ src/com/csa/entity/Bowl.java
@@ -2,26 +2,194 @@ package com.csa.entity;
 
 public class Bowl {
 
-	String bowler;
+	public String bowler;
 	
-	int overNumber;
-	int bowlnumber;
-	int ligitBowlnumber;
+	public int overNumber;
+	public int bowlnumber;
+	public int ligitBowlnumber;
 	
-	String batsman;
-	String nonStriker;
+	public String batsman;
+	public String nonStriker;
 	
-	int runs;
-	int extras;
-	int extraType;
+	public int runs;
+	public int extras;
+	public int extraType;
 	
-	int totalRuns;
-	int isWicket;
+	public int totalRuns;
+	public int isWicket;
 	
-	Wicket wicket;
+	public Wicket wicket;
 	
 	public Bowl() {
 		// TODO Auto-generated constructor stub
 	}
+
+    /**
+     * @return the bowler
+     */
+    public String getBowler() {
+        return bowler;
+    }
+
+    /**
+     * @param bowler the bowler to set
+     */
+    public void setBowler(String bowler) {
+        this.bowler = bowler;
+    }
+
+    /**
+     * @return the overNumber
+     */
+    public int getOverNumber() {
+        return overNumber;
+    }
+
+    /**
+     * @param overNumber the overNumber to set
+     */
+    public void setOverNumber(int overNumber) {
+        this.overNumber = overNumber;
+    }
+
+    /**
+     * @return the bowlnumber
+     */
+    public int getBowlnumber() {
+        return bowlnumber;
+    }
+
+    /**
+     * @param bowlnumber the bowlnumber to set
+     */
+    public void setBowlnumber(int bowlnumber) {
+        this.bowlnumber = bowlnumber;
+    }
+
+    /**
+     * @return the ligitBowlnumber
+     */
+    public int getLigitBowlnumber() {
+        return ligitBowlnumber;
+    }
+
+    /**
+     * @param ligitBowlnumber the ligitBowlnumber to set
+     */
+    public void setLigitBowlnumber(int ligitBowlnumber) {
+        this.ligitBowlnumber = ligitBowlnumber;
+    }
+
+    /**
+     * @return the batsman
+     */
+    public String getBatsman() {
+        return batsman;
+    }
+
+    /**
+     * @param batsman the batsman to set
+     */
+    public void setBatsman(String batsman) {
+        this.batsman = batsman;
+    }
+
+    /**
+     * @return the nonStriker
+     */
+    public String getNonStriker() {
+        return nonStriker;
+    }
+
+    /**
+     * @param nonStriker the nonStriker to set
+     */
+    public void setNonStriker(String nonStriker) {
+        this.nonStriker = nonStriker;
+    }
+
+    /**
+     * @return the runs
+     */
+    public int getRuns() {
+        return runs;
+    }
+
+    /**
+     * @param runs the runs to set
+     */
+    public void setRuns(int runs) {
+        this.runs = runs;
+    }
+
+    /**
+     * @return the extras
+     */
+    public int getExtras() {
+        return extras;
+    }
+
+    /**
+     * @param extras the extras to set
+     */
+    public void setExtras(int extras) {
+        this.extras = extras;
+    }
+
+    /**
+     * @return the extraType
+     */
+    public int getExtraType() {
+        return extraType;
+    }
+
+    /**
+     * @param extraType the extraType to set
+     */
+    public void setExtraType(int extraType) {
+        this.extraType = extraType;
+    }
+
+    /**
+     * @return the totalRuns
+     */
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    /**
+     * @param totalRuns the totalRuns to set
+     */
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    /**
+     * @return the isWicket
+     */
+    public int getIsWicket() {
+        return isWicket;
+    }
+
+    /**
+     * @param isWicket the isWicket to set
+     */
+    public void setIsWicket(int isWicket) {
+        this.isWicket = isWicket;
+    }
+
+    /**
+     * @return the wicket
+     */
+    public Wicket getWicket() {
+        return wicket;
+    }
+
+    /**
+     * @param wicket the wicket to set
+     */
+    public void setWicket(Wicket wicket) {
+        this.wicket = wicket;
+    }
 
 }
