1a1915e0995ed489b5d22df7a2157656e6133146
sanjya <sanjayanuwanaj@gmail.com>
2016-03-17 15:20:56 +0530

    test

--- src/com/csa/entity/Innings.java
+++ src/com/csa/entity/Innings.java
@@ -4,14 +4,70 @@ import java.util.Map;
 
 public class Innings {
 
-	Team battingTeam;
-	Team fieldingTeam;
-	int numberOfOvers;
+	public Team battingTeam;
+	public Team fieldingTeam;
+	public int numberOfOvers;
 	
-	Map<Integer,Bowl> deliveries; 
+	public Map<Integer,Bowl> deliveries; 
 	
 	public Innings() {
 		// TODO Auto-generated constructor stub
 	}
+
+    /**
+     * @return the battingTeam
+     */
+    public Team getBattingTeam() {
+        return battingTeam;
+    }
+
+    /**
+     * @param battingTeam the battingTeam to set
+     */
+    public void setBattingTeam(Team battingTeam) {
+        this.battingTeam = battingTeam;
+    }
+
+    /**
+     * @return the fieldingTeam
+     */
+    public Team getFieldingTeam() {
+        return fieldingTeam;
+    }
+
+    /**
+     * @param fieldingTeam the fieldingTeam to set
+     */
+    public void setFieldingTeam(Team fieldingTeam) {
+        this.fieldingTeam = fieldingTeam;
+    }
+
+    /**
+     * @return the numberOfOvers
+     */
+    public int getNumberOfOvers() {
+        return numberOfOvers;
+    }
+
+    /**
+     * @param numberOfOvers the numberOfOvers to set
+     */
+    public void setNumberOfOvers(int numberOfOvers) {
+        this.numberOfOvers = numberOfOvers;
+    }
+
+    /**
+     * @return the deliveries
+     */
+    public Map<Integer,Bowl> getDeliveries() {
+        return deliveries;
+    }
+
+    /**
+     * @param deliveries the deliveries to set
+     */
+    public void setDeliveries(Map<Integer,Bowl> deliveries) {
+        this.deliveries = deliveries;
+    }
 
 }
--- src/com/csa/entity/Team.java
+++ src/com/csa/entity/Team.java
@@ -4,14 +4,84 @@ import java.util.Map;
 
 public class Team {
 
-	int teamId;
-	String teamName;
-	String captain;
-	String wicketKeper;
-	Map<Integer,String> Playing11;  
+	public int teamId;
+	public String teamName;
+	public String captain;
+	public String wicketKeper;
+	public Map<Integer,String> Playing11;  
 	
 	public Team() {
 		// TODO Auto-generated constructor stub
 	}
+
+    /**
+     * @return the teamId
+     */
+    public int getTeamId() {
+        return teamId;
+    }
+
+    /**
+     * @param teamId the teamId to set
+     */
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    /**
+     * @return the teamName
+     */
+    public String getTeamName() {
+        return teamName;
+    }
+
+    /**
+     * @param teamName the teamName to set
+     */
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    /**
+     * @return the captain
+     */
+    public String getCaptain() {
+        return captain;
+    }
+
+    /**
+     * @param captain the captain to set
+     */
+    public void setCaptain(String captain) {
+        this.captain = captain;
+    }
+
+    /**
+     * @return the wicketKeper
+     */
+    public String getWicketKeper() {
+        return wicketKeper;
+    }
+
+    /**
+     * @param wicketKeper the wicketKeper to set
+     */
+    public void setWicketKeper(String wicketKeper) {
+        this.wicketKeper = wicketKeper;
+    }
+
+    /**
+     * @return the Playing11
+     */
+    public Map<Integer,String> getPlaying11() {
+        return Playing11;
+    }
+
+    /**
+     * @param Playing11 the Playing11 to set
+     */
+    public void setPlaying11(Map<Integer,String> Playing11) {
+        this.Playing11 = Playing11;
+    }
 
 }
